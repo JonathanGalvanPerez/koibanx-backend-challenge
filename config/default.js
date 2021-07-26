7abc6903818599df6ad5fff373e784702f2a74c9
Jonathan Galvan Perez <jonathangalvanperez@gmail.com>
2021-07-25 21:04:00 -0300

    add eslint and env configs

--- config/default.js
+++ config/default.js
@@ -1,4 +1,4 @@
-module.exports = {
+const development_config = {
   'port': 3000,
   'mongodb': {
     'username': process.env.DATABASE_USERNAME,
@@ -9,3 +9,17 @@ module.exports = {
     'address': process.env.DATABASE_HOST + ':' + process.env.DATABASE_PORT
   },
 };
+
+const test_config = {
+  'port': 3000,
+  'mongodb': {
+    'username': process.env.DATABASE_USERNAME,
+    'password': process.env.DATABASE_PASSWORD,
+    'host': process.env.DATABASE_HOST,
+    'port': process.env.DATABASE_PORT,
+    'dbname': process.env.TEST_DATABASE_DBNAME,
+    'address': process.env.DATABASE_HOST + ':' + process.env.DATABASE_PORT
+  },
+};
+
+module.exports = process.env.NODE_ENV === 'test' ? test_config : development_config;
